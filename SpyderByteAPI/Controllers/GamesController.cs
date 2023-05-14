82b759aab7656f7123334f720ba541e87d3410b5
Oliver <olivernicholls91@hotmail.com>
2023-05-14 18:24:09 +0100

    Temporarily stubbing out Games accessor.

--- SpyderByteAPI/Controllers/GamesController.cs
+++ SpyderByteAPI/Controllers/GamesController.cs
@@ -25,124 +25,124 @@ namespace SpyderByteAPI.Controllers
         //  If costs are low, then this can be exposed (with credentials).
 
 
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> Get()
-        {
-            IDataResponse<IList<Game>?> response = await gamesAccessor.GetAllAsync();
+        //[HttpGet]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //public async Task<IActionResult> Get()
+        //{
+        //    IDataResponse<IList<Game>?> response = await gamesAccessor.GetAllAsync();
 
-            if (response.Result == ModelResult.OK)
-            {
-                // 200
-                return Ok(response.Data);
-            }
-            else
-            {
-                // 500
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
+        //    if (response.Result == ModelResult.OK)
+        //    {
+        //        // 200
+        //        return Ok(response.Data);
+        //    }
+        //    else
+        //    {
+        //        // 500
+        //        return StatusCode(StatusCodes.Status500InternalServerError);
+        //    }
+        //}
 
-        [HttpGet("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetGame(int id)
-        {
-            IDataResponse<Game?> response = await gamesAccessor.GetSingleAsync(id);
+        //[HttpGet("{id}")]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //public async Task<IActionResult> GetGame(int id)
+        //{
+        //    IDataResponse<Game?> response = await gamesAccessor.GetSingleAsync(id);
 
-            if (response.Result == ModelResult.OK)
-            {
-                // 200
-                return Ok(response.Data);
-            }
-            else if (response.Result == ModelResult.NotFound)
-            {
-                // 404
-                return NotFound();
-            }
-            else
-            {
-                // 500
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
+        //    if (response.Result == ModelResult.OK)
+        //    {
+        //        // 200
+        //        return Ok(response.Data);
+        //    }
+        //    else if (response.Result == ModelResult.NotFound)
+        //    {
+        //        // 404
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        // 500
+        //        return StatusCode(StatusCodes.Status500InternalServerError);
+        //    }
+        //}
 
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> Post([FromBody] PostGame game)
-        {
-            IDataResponse<Game?> response = await gamesAccessor.PostAsync(game);
+        //[HttpPost]
+        //[ProducesResponseType(StatusCodes.Status201Created)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //public async Task<IActionResult> Post([FromBody] PostGame game)
+        //{
+        //    IDataResponse<Game?> response = await gamesAccessor.PostAsync(game);
 
-            if (response.Result == ModelResult.Created)
-            {
-                // 201
-                return CreatedAtAction(nameof(GetGame), new { id = response?.Data?.Id }, response?.Data);
-            }
-            else if (response.Result == ModelResult.AlreadyExists)
-            {
-                // 400
-                return BadRequest(modelResources.GetResource(ModelResult.AlreadyExists));
-            }
-            else
-            {
-                // 500
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
+        //    if (response.Result == ModelResult.Created)
+        //    {
+        //        // 201
+        //        return CreatedAtAction(nameof(GetGame), new { id = response?.Data?.Id }, response?.Data);
+        //    }
+        //    else if (response.Result == ModelResult.AlreadyExists)
+        //    {
+        //        // 400
+        //        return BadRequest(modelResources.GetResource(ModelResult.AlreadyExists));
+        //    }
+        //    else
+        //    {
+        //        // 500
+        //        return StatusCode(StatusCodes.Status500InternalServerError);
+        //    }
+        //}
 
-        [HttpPatch]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> Patch([FromBody] PatchGame game)
-        {
-            IDataResponse<Game?> response = await gamesAccessor.PatchAsync(game);
+        //[HttpPatch]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //public async Task<IActionResult> Patch([FromBody] PatchGame game)
+        //{
+        //    IDataResponse<Game?> response = await gamesAccessor.PatchAsync(game);
 
-            if (response.Result == ModelResult.OK)
-            {
-                // 200
-                return Ok(response.Data);
-            }
-            else if (response.Result == ModelResult.NotFound)
-            {
-                // 404
-                return NotFound();
-            }
-            else
-            {
-                // 500
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
+        //    if (response.Result == ModelResult.OK)
+        //    {
+        //        // 200
+        //        return Ok(response.Data);
+        //    }
+        //    else if (response.Result == ModelResult.NotFound)
+        //    {
+        //        // 404
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        // 500
+        //        return StatusCode(StatusCodes.Status500InternalServerError);
+        //    }
+        //}
 
-        [HttpDelete("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> Delete(int id)
-        {
-            IDataResponse<Game?> response = await gamesAccessor.DeleteAsync(id);
+        //[HttpDelete("{id}")]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    IDataResponse<Game?> response = await gamesAccessor.DeleteAsync(id);
 
-            if (response.Result == ModelResult.OK)
-            {
-                // 200
-                return Ok(response.Data);
-            }
-            else if (response.Result == ModelResult.NotFound)
-            {
-                // 404
-                return NotFound();
-            }
-            else
-            {
-                // 500
-                return StatusCode(StatusCodes.Status500InternalServerError);
-            }
-        }
+        //    if (response.Result == ModelResult.OK)
+        //    {
+        //        // 200
+        //        return Ok(response.Data);
+        //    }
+        //    else if (response.Result == ModelResult.NotFound)
+        //    {
+        //        // 404
+        //        return NotFound();
+        //    }
+        //    else
+        //    {
+        //        // 500
+        //        return StatusCode(StatusCodes.Status500InternalServerError);
+        //    }
+        //}
     }
 }
