5f6be75dae2972ab79d63f258ca9a093ef6c116f
ojn17041991 <40460862+ojn17041991@users.noreply.github.com>
2023-10-02 08:03:46 +0100

    Fixed middleware to extract JSON body from request.

--- SpyderByteAPI/Middleware/RequestBodyToInsightMiddleware.cs
+++ SpyderByteAPI/Middleware/RequestBodyToInsightMiddleware.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.ApplicationInsights.DataContracts;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace SpyderByteAPI.Middleware
 {
@@ -15,6 +17,8 @@ namespace SpyderByteAPI.Middleware
                 {
                     if (request.ContentLength > 0 && (request.Method == HttpMethods.Post || request.Method == HttpMethods.Put || request.Method == HttpMethods.Patch))
                     {
+                        var properties = new Dictionary<string, string>();
+
                         request.EnableBuffering();
                         request.Body.Position = 0;
 
@@ -22,15 +26,42 @@ namespace SpyderByteAPI.Middleware
                         {
                             string requestBody  = await reader.ReadToEndAsync();
 
-                            var index = requestBody.IndexOf("Content-Type: image/");
-                            if (index != -1)
-                            {
-                                var endIndex = requestBody.IndexOf("---", index);
-                                requestBody.Remove(index, endIndex - index);
-                            }
+                            var regex = new Regex(@"; name=""[A-Za-z0-9_]+""");
+                            var matches = regex.Matches(requestBody);
 
-                            requestTelemetry.Properties.Add("RequestBody", requestBody);
+                            foreach (var match in matches)
+                            {
+                                var matchValue = match?.ToString();
+                                if (matchValue == null) continue;
+
+                                var nameStr = @"; name=""";
+                                var nameStartIdx = matchValue.IndexOf(nameStr) + nameStr.Length;
+                                var nameEndIdx = matchValue.IndexOf(@"""", nameStartIdx);
+                                var name = matchValue.Substring(nameStartIdx, nameEndIdx - nameStartIdx);
+
+                                var valueStr = @$"{nameStr}{name}""";
+                                var valueStartIdx = requestBody.IndexOf(valueStr) + valueStr.Length;
+                                string value;
+
+                                if (name.Contains("image"))
+                                {
+                                    var fileNameStr = @"filename=""";
+                                    var fileNameStartIdx = requestBody.IndexOf(fileNameStr, valueStartIdx) + fileNameStr.Length;
+                                    var fileNameEndIdx = requestBody.IndexOf(@"""", fileNameStartIdx);
+                                    value = requestBody.Substring(fileNameStartIdx, fileNameEndIdx - fileNameStartIdx);
+                                }
+                                else
+                                {
+                                    var valueEndIdx = requestBody.IndexOf(@"---", valueStartIdx);
+                                    value = requestBody.Substring(valueStartIdx, valueEndIdx - valueStartIdx).Trim();
+                                }
+
+                                properties.Add(name, value);
+                            }
                         }
+
+                        var propertiesJson = JsonConvert.SerializeObject(properties, Formatting.Indented);
+                        requestTelemetry.Properties.Add("requestBody", propertiesJson);
 
                         request.Body.Position = 0;
                     }
