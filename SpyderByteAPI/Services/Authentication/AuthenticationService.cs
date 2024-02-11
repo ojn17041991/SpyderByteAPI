526a48c784f67f3fd6c070c3c75a3cad2058229b
ojn17041991 <40460862+ojn17041991@users.noreply.github.com>
2024-02-11 17:32:51 +0000

    Adding logging to authentication service.

--- SpyderByteAPI/Services/Authentication/AuthenticationService.cs
+++ SpyderByteAPI/Services/Authentication/AuthenticationService.cs
@@ -12,11 +12,13 @@ namespace SpyderByteAPI.Services.Auth
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly ILogger<AuthenticationService> logger;
         private readonly IConfiguration configuration;
         private readonly TokenEncoder tokenEncoder;
 
-        public AuthenticationService(IConfiguration configuration, TokenEncoder tokenEncoder)
+        public AuthenticationService(ILogger<AuthenticationService> logger, IConfiguration configuration, TokenEncoder tokenEncoder)
         {
+            this.logger = logger;
             this.configuration = configuration;
             this.tokenEncoder = tokenEncoder;
         }
@@ -42,17 +44,29 @@ namespace SpyderByteAPI.Services.Auth
             }
             else
             {
+                logger.LogError($"Failed to authenticate {login.UserName} user. Invalid credentials.");
                 return new DataResponse<string>(string.Empty, ModelResult.Unauthorized);
             }
 
             var token = tokenEncoder.Encode(claims);
+            if (token.IsNullOrEmpty())
+            {
+                logger.LogError($"Failed to authenticate {login.UserName} user. Unable to generate token.");
+                return new DataResponse<string>(string.Empty, ModelResult.Error);
+            }
+
+            logger.LogInformation($"Authenticated {login.UserName} user.");
             return new DataResponse<string>(token, ModelResult.OK);
         }
 
         public IDataResponse<string> Deauthenticate(HttpContext context)
         {
             var token = TokenExtractor.GetTokenFromHttpContext(context);
-            if (token.IsNullOrEmpty()) return new DataResponse<string>(string.Empty, ModelResult.Error);
+            if (token.IsNullOrEmpty())
+            {
+                logger.LogError($"Failed to deauthenticate user. Unable to find authorization token.");
+                return new DataResponse<string>(string.Empty, ModelResult.Error);
+            }
 
             TokenBlacklister.AddTokenToBlacklist(token);
             return new DataResponse<string>(token, ModelResult.OK);
@@ -61,13 +75,25 @@ namespace SpyderByteAPI.Services.Auth
         public IDataResponse<string> Refresh(HttpContext context)
         {
             var token = TokenExtractor.GetTokenFromHttpContext(context);
-            if (token.IsNullOrEmpty()) return new DataResponse<string>(string.Empty, ModelResult.Error);
+            if (token.IsNullOrEmpty())
+            {
+                logger.LogError($"Failed to refresh token. Unable to find authorization token.");
+                return new DataResponse<string>(string.Empty, ModelResult.Error);
+            }
 
             var claims = tokenEncoder.Decode(token);
-            if (claims.IsNullOrEmpty()) return new DataResponse<string>(string.Empty, ModelResult.Error);
+            if (claims.IsNullOrEmpty())
+            {
+                logger.LogError($"Failed to refresh token. Unable to extract claims from token.");
+                return new DataResponse<string>(string.Empty, ModelResult.Error);
+            }
 
             var refreshToken = tokenEncoder.Encode(claims);
-            if (refreshToken.IsNullOrEmpty()) return new DataResponse<string>(string.Empty, ModelResult.Error);
+            if (refreshToken.IsNullOrEmpty())
+            {
+                logger.LogError($"Failed to refresh token. Unable to generate token.");
+                return new DataResponse<string>(string.Empty, ModelResult.Error);
+            }
 
             TokenBlacklister.AddTokenToBlacklist(token);
             return new DataResponse<string>(refreshToken, ModelResult.OK);
