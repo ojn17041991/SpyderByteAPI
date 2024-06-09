d71dca984a99fde4616850279d85e11991be51ae
Oliver Nicholls <olivernicholls91@hotmail.com>
2024-06-09 15:48:06 +0100

    Added PasswordService unit tests.

---
+++ SpyderByteAPITest/Services/PasswordServiceTests/Helpers/PasswordServiceHelper.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.Configuration;
+using SpyderByteServices.Services.Password;
+
+namespace SpyderByteTest.Services.PasswordServiceTests.Helpers
+{
+    public class PasswordServiceHelper
+    {
+        public PasswordService Service;
+
+        private readonly Fixture _fixture;
+
+        public PasswordServiceHelper()
+        {
+            _fixture = new Fixture();
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            Dictionary<string, string?> configurationContents = new()
+            {
+                { "Encryption:Argon2:DegreesOfParallelism", "1" },
+                { "Encryption:Argon2:MemorySize", "1024" },
+                { "Encryption:Argon2:Iterations", "1" }
+            };
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configurationContents)
+                .Build();
+
+            Service = new PasswordService(configuration);
+        }
+    }
+}
