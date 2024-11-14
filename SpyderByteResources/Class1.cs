e2ae05f9036f5f25cc68478ccec3059dfad77883
Oliver Nicholls <olivernicholls91@hotmail.com>
2024-11-14 13:38:57 +0000

    Switching project over.

---
+++ SpyderByteResources/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpyderByteResources
+{
+    public static  class Class1
+    {
+        public static string Name { get; set; } = "Hello";
+    }
+}
